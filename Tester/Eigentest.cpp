b62c8d2788e22cee51e6ba927593a71fbf1b32b1
Luka D <luka.duplancic@gmail.com>
2022-07-03 12:44:33 +0200

    tensor operations done, backup before implementing them in conv layer

--- Tester/Eigentest.cpp
+++ Tester/Eigentest.cpp
@@ -34,6 +34,32 @@ template<typename T, int dims> void print(Eigen::Tensor<T, dims>& tensor, bool f
 	}
 }
 
+template<typename T, int dims> void print(Eigen::TensorMap<Eigen::Tensor<double, dims>>& tensor, bool flatten_2d, int d = 0, Eigen::array<ptrdiff_t, dims>& arr = *(new Eigen::array<ptrdiff_t, dims>)) {
+	if (d == dims) {
+		cout << tensor(arr);
+		if (arr[dims - 1] < tensor.dimension(dims - 1) - 1) cout << ", ";
+		return;
+	}
+
+	for (int i = 0; i < tensor.dimension(d); i++) {
+		arr[d] = i;
+		if (d < dims - 1) {
+			if (i > 0) for (int _ = 0; _ < d; _++) cout << ' ';
+			cout << '[';
+		}
+
+		print<double, dims>(tensor, flatten_2d, d + 1, arr);
+
+		if (d < dims - 1) {
+			cout << ']';
+			if (flatten_2d && d == dims - 2) {
+				if (i < tensor.dimension(d) - 1) cout << ", ";
+			}
+			else if (i < tensor.dimension(d) - 1) cout << '\n';
+		}
+	}
+}
+
 template<typename T, int dims> void printrev(Eigen::Tensor<T, dims>& tensor, int d = 0, Eigen::array<ptrdiff_t, dims>& arr = *(new Eigen::array<ptrdiff_t, dims>)) {
 	if (d == dims) {
 		cout << tensor(arr);
@@ -58,7 +84,7 @@ template<typename T, int dims> void printrev(Eigen::Tensor<T, dims>& tensor, int
 	}
 }
 
-int input_r = 2, input_c = 2, input_d = 2;
+int input_r = 3, input_c = 3, input_d = 2;
 int kernel_r = 2, kernel_c = 2, kernel_d = 2;
 
 Eigen::Tensor<double, 3> in(input_d, input_r, input_c);
@@ -66,27 +92,41 @@ Eigen::Tensor<double, 3> kernel(kernel_r, kernel_c, kernel_d);
 
 int main() {
 	in.setValues({
-		{{1, 1},
-		{1, 1}},
+		{{1, 2, 3},
+		{4, 5, 6},
+		{7, 8, 9}},
 
-		{{1, 0},
-		{0, 1}}
+		{{1, 1, 1},
+		{1, 0, 0},
+		{0, 0, 1}}
 		});
 	kernel.setValues({
-		{{1, 1}}, {{1, 0}},
-		{{1, 0}}, {{1, 1}}
+		{{1, 1}, {1, 0}},
+		{{1, 1}, {1, 1}}
 		});
 
-	auto res = Nets::Convolve(in, kernel, Eigen::PADDING_SAME);
-	print(res);
+	auto temp = Nets::Tensor3DToRowVec(in);
+	auto temp2 = Nets::RowVecToTensor3D(temp);
+	print(temp2, false);
+	
+	Eigen::RowVectorXd rvin(2 * 3 * 3 + 3);
+	rvin << 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 1, 1, 1, 0, 0, 0, 0, 1, 2, 3, 3;
 
-	cout << "Input tensor:\n";
+	cout << "\n\n";
+
+	auto res = Nets::Convolve(in, kernel, Eigen::PADDING_SAME);
+	print(res, false);
+
+	/*cout << "Input tensor:\n";
 	print(in);
 	cout << "\n\n";
 
 	cout << "Image patches:\n";
 	Eigen::Tensor<double, 4> patches = in.extract_image_patches(kernel_r, kernel_c);
 	print(patches, true);
+	Eigen::Tensor<double, 4> psh = patches.shuffle(Eigen::array<ptrdiff_t, 4>{3, 0, 1, 2});
+	cout << '\n';
+	print(psh, false);
 	cout << "\n\n";
 
 	long long patches_total = 1;
@@ -97,7 +137,7 @@ int main() {
 	cout << "\n\n";
 
 	cout << "Reshaped patches:\n";
-	Eigen::Tensor<double, 2> reshaped1 = patches.reshape(Eigen::array<ptrdiff_t, 2>({ patches_total / (kernel_r * kernel_c), kernel_r * kernel_c}));
+	Eigen::Tensor<double, 2> reshaped1 = psh.reshape(Eigen::array<ptrdiff_t, 2>({ patches_total / (kernel_r * kernel_c), kernel_r * kernel_c}));
 	print(reshaped1, false);
 	cout << "\n\n";
 
@@ -116,10 +156,11 @@ int main() {
 	cout << "\n\n";
 
 	cout << "Final reshaping:\n";
-	Eigen::Tensor<double, 4> out = contraction.reshape(Eigen::array<ptrdiff_t, 4>{input_d, input_r, input_c, kernel_d});
+	Eigen::Tensor<double, 3> out = contraction.shuffle(Eigen::array<ptrdiff_t, 2>{1, 0}).reshape(Eigen::array<ptrdiff_t, 4>{input_d, input_r, input_c, kernel_d})
+		.shuffle(Eigen::array<ptrdiff_t, 4>{0, 3, 1, 2}).reshape(Eigen::array<ptrdiff_t, 3>{input_d* kernel_d, input_r, input_c});
 
-	print(out, true);
-	cout << "\n\n";
+	print(out, false);
+	cout << "\n\n";*/
 
 	return 0;
 }
--- neuralnet/conv_layer.h
+++ neuralnet/conv_layer.h
@@ -8,7 +8,7 @@ namespace Nets {
 	const int CONV = 3;
 
 	template<typename Scalar>
-	inline Eigen::Tensor<Scalar, 4> Convolve(const Eigen::Tensor<Scalar, 3>& input, const Eigen::Tensor<Scalar, 3>& kernel, Eigen::PaddingType padding = Eigen::PADDING_SAME) {
+	inline Eigen::Tensor<Scalar, 3> Convolve(const Eigen::Tensor<Scalar, 3>& input, const Eigen::Tensor<Scalar, 3>& kernel, Eigen::PaddingType padding = Eigen::PADDING_SAME) {
 		int input_d = input.dimension(0), input_w = input.dimension(1), input_h = input.dimension(2);
 		int kernel_w = kernel.dimension(0), kernel_h = kernel.dimension(1), kernel_d = kernel.dimension(2);
 
@@ -26,11 +26,30 @@ namespace Nets {
 
 		return input
 			.extract_image_patches(kernel_w, kernel_h, 1i64, 1i64, 1i64, 1i64, padding)
-			.reshape(Eigen::array<ptrdiff_t, 2>{input_d * output_w * output_h, kernel_w * kernel_h})
+			.shuffle(Eigen::array<ptrdiff_t, 4>{3, 0, 1, 2})
+			.reshape(Eigen::array<ptrdiff_t, 2>{input_d* output_w* output_h, kernel_w* kernel_h})
 			.contract(
-				kernel.reshape(Eigen::array<ptrdiff_t, 2>{kernel_w * kernel_h, kernel_d}),
+				kernel.reshape(Eigen::array<ptrdiff_t, 2>{kernel_w* kernel_h, kernel_d}),
 				Eigen::array<Eigen::IndexPair<int>, 1>{Eigen::IndexPair<int>{1, 0}})
-			.reshape(Eigen::array<ptrdiff_t, 4>{output_d, output_w, output_h, kernel_d});
+			.shuffle(Eigen::array<ptrdiff_t, 2>{1, 0})
+			.reshape(Eigen::array<ptrdiff_t, 4>{output_d, output_w, output_h, kernel_d})
+			.shuffle(Eigen::array<ptrdiff_t, 4>{0, 3, 1, 2})
+			.reshape(Eigen::array<ptrdiff_t, 3>{ output_d* kernel_d, output_w, output_h });
+	}
+
+	template<typename NumType> 
+	inline Eigen::RowVectorX<NumType> Tensor3DToRowVec(Eigen::Tensor<NumType, 3>& tensor) {
+		int tensor_sz = tensor.dimension(0) * tensor.dimension(1) * tensor.dimension(2);
+		Eigen::RowVectorX<NumType> rv(tensor_sz + 3);
+		rv.head(tensor_sz) = Eigen::Map<Eigen::RowVectorX<NumType>>(tensor.data(), tensor_sz);
+		rv.tail(3) = Eigen::RowVector3<NumType>{ (NumType)tensor.dimension(0), (NumType)tensor.dimension(1), (NumType)tensor.dimension(2) };
+		return rv;
+	}
+
+	template<typename NumType>
+	inline Eigen::Tensor<NumType, 3> RowVecToTensor3D(Eigen::RowVectorX<NumType>& rv) {
+		int z = rv(rv.size() - 3), x = rv(rv.size() - 2), y = rv(rv.size() - 1);
+		return Eigen::TensorMap<Eigen::Tensor<NumType, 3>>(rv.data(), z, x, y);
 	}
 
 	class ConvL : public LayerCRTP<ConvL> {
